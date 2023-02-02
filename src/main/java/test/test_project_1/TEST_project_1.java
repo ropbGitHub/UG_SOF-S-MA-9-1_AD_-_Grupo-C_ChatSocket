b333b61a1c280909e5a1b4fe110b2deb98a08e34
Renán Pérez Balladares <64661058+ropbGitHub@users.noreply.github.com>
2023-02-01 22:40:11 -0500

    Commit to the Commit

---
+++ src/main/java/test/test_project_1/TEST_project_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.test_project_1;
+
+/**
+ *
+ * @author USER
+ */
+public class TEST_project_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
